9bdf5b3cb6906fb4869004c772644e3b4b5bf1e9
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-06-20 21:09:42 +0300

    lesson #16

--- src/main/java/com/yuriy/traning/messenger/service/MessageService.java
+++ src/main/java/com/yuriy/traning/messenger/service/MessageService.java
@@ -2,11 +2,21 @@ package com.yuriy.traning.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import com.yuriy.traning.messenger.model.*;
+import com.yuriy.traning.messenger.database.DatabaseClass;
+import com.yuriy.traning.messenger.model.Message;
 
 public class MessageService {
 	
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1L, "Hello World!", "Yuriy"));
+		messages.put(2L, new Message(2L, "Some Text!", "Homer"));
+	}
+	
+	/*
 	public List<Message> getAllMessages() {
 		Message m1 = new Message(1L, "Hello World!", "Yuriy");
 		Message m2 = new Message(2L, "Some Text!", "Homer");
@@ -16,5 +26,32 @@ public class MessageService {
 		list.add(m2);
 		return list;
 	}
+	*/
+	
+	public List<Message> getAllMessages() {
+		return new ArrayList<Message>(messages.values()); 
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if (message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
+	}	
 
 }
