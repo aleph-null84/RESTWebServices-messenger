481be22c054f595fead78da22b8bb169d67daa57
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-06-28 18:33:18 +0300

    lesson #28

--- src/main/java/com/yuriy/traning/messenger/exception/GenericExceptionMapper.java
+++ src/main/java/com/yuriy/traning/messenger/exception/GenericExceptionMapper.java
@@ -7,7 +7,7 @@ import javax.ws.rs.ext.Provider;
 
 import com.yuriy.traning.messenger.model.ErrorMessage;
 
-@Provider
+//@Provider
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 
 	@Override
